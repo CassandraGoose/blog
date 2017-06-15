16a1451dd51e2ddc1e9b72776379000dc637746a
Cassandra Torske <cassandra.torske@gmail.com>
2017-06-15 12:01:56 -0600

    heyyy

--- db/knex.js
+++ db/knex.js
@@ -1,6 +1,6 @@
 var environment = process.env.NODE_ENV || 'development';
 console.log(environment)
-var knexConfig = require('./knexfile')
+var knexConfig = require('../knexfile')
 var config = knexConfig[environment];
 var knex = require('knex');
 var knexConnection = knex(config);
