ce8959d2276fa23abb99f11617d6b34119da637a
Cassandra Torske <cassandra.torske@gmail.com>
2017-06-19 11:34:31 -0600

    frands

--- seeds/01_people.js
+++ seeds/01_people.js
@@ -12,21 +12,27 @@ return knex.raw('delete from people; alter sequence people_id_seq restart with 4
         username: 'sexhaver',
         email: 'jim@jim.com',
         password: bcrypt.hashSync('password123', salt),
-        photo_url: 'https://washington-org.s3.amazonaws.com/s3fs-public/styles/navigation_block/public/homepage_hero_shot_free_things_pandas_0.jpg?itok=23cgXDWj'
+        photo_url: 'https://washington-org.s3.amazonaws.com/s3fs-public/styles/navigation_block/public/homepage_hero_shot_free_things_pandas_0.jpg?itok=23cgXDWj',
+        friend1: 'noteableSlug',
+        friend2: 'pringles'
       },
       {
         id: 2,
         username: 'notableSlug',
         email: 'jeffrey@j.com',
         password: bcrypt.hashSync('password123', salt),
-        photo_url: 'https://washington-org.s3.amazonaws.com/s3fs-public/styles/navigation_block/public/homepage_hero_shot_free_things_pandas_0.jpg?itok=23cgXDWj'
+        photo_url: 'https://washington-org.s3.amazonaws.com/s3fs-public/styles/navigation_block/public/homepage_hero_shot_free_things_pandas_0.jpg?itok=23cgXDWj',
+        friend1: 'sexhaver',
+        friend2: 'pringles'
       },
       {
         id: 3,
         username: 'pringles',
         email: 'jim@poop.com',
         password: bcrypt.hashSync('password123', salt),
-        photo_url: 'https://washington-org.s3.amazonaws.com/s3fs-public/styles/navigation_block/public/homepage_hero_shot_free_things_pandas_0.jpg?itok=23cgXDWj'
+        photo_url: 'https://washington-org.s3.amazonaws.com/s3fs-public/styles/navigation_block/public/homepage_hero_shot_free_things_pandas_0.jpg?itok=23cgXDWj',
+        friend1: 'noteableSlug',
+        friend2: 'sexhaver'
       }]
       return knex('people').insert(somePeeps)
 
